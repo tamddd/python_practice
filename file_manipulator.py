ae4b902f5cd14f7d330a70682a3e8b9cb2fa5d02
tamd <daiki1106tamu@gmail.com>
2023-02-19 23:34:01 +0900

    k

---
+++ file_manipulator.py
@@ -0,0 +1,20 @@
+def reverse(input_path, output_path):
+    pass
+
+
+def copy(input_path, output_path):
+    pass
+
+def duplicate_contents(input_path, n):
+    pass
+
+def replace_string(input_path, output_path):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
